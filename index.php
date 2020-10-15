fb2a9bcf7c6e56a04deef88549564a45a1ec46be
Snoet_ <tycho.brakenhoff@ictmbo.nl>
2020-10-15 15:08:17 +0200

    Added Index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>Kras Hosting</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<a href="/Pages/LoginPage/login.php">Login!</a>
+
+
+
+
+</body>
+</html>
+
