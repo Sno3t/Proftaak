074772ad658bb7e43c633a773d5ac0b8c3cbdb1f
Snoet_ <tycho.brakenhoff@ictmbo.nl>
2020-10-15 16:14:45 +0200

    Updated Index.php

--- index.php
+++ index.php
@@ -3,12 +3,36 @@
 <head>
     <title>Kras Hosting</title>
     <meta charset="utf-8">
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 2px;
+
+        }
+    </style>
 </head>
 <body>
 
 <a href="/Pages/LoginPage/login.php">Login!</a>
 
+<table>
+    <thead>
+    <tr>
+        <td>Student</td>
+        <td>Engels</td>
+        <td>Nederlands</td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
 
+    </tr>
+    </tbody>
+</table>
 
 
 </body>
