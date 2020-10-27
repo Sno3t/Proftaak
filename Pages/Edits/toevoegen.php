f4bfbed1ccbe7fce715d5896230f5b641f9dcf23
Sno3t <tycho.brakenhoff@ictmbo.nl>
2020-10-27 23:00:00 +0100

    Added add funtion not yet working

--- Pages/Edits/toevoegen.php
+++ Pages/Edits/toevoegen.php
@@ -1,3 +1,27 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+<body>
+    <form action="" method="post">
+
+        Cijfer:
+        <input name="Cijfer"> <br>
+        Datum:
+        <input name="Datum"> <br>
+        Toets naam:
+        <input name="Toetsnaam"> <br>
+        Studenten ID:
+        <input name="$studentenID"> <br>
+        Leraren ID:
+        <input name="$LerarenID"> <br>
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
+
 <?php
 require_once("../../Classes/MysqlConnection.php");
 $mysql = new MysqlConnection();
@@ -5,9 +29,18 @@ $mysql = new MysqlConnection();
 if (isset($_GET['Engels'])) {
 
     try {
-        require_once("Engels.php");
-        $eng = new Engels();
-        $eng->EngelsResultaten($mysql->connect());
+        $stmt = $mysql->prepare("INSERT INTO `engels`(`ID`, `Cijfer`, `Datum`, `toetsNaam`, `Studenten_ID`, `Leraren_ID`) VALUES (,?,?,?,?,?);");
+        $stmt->bind_param('sssss', $Cijfer, $Datum, $Toetsnaam, $studentenID, $LerarenID);
+
+
+        $Cijfer = $_POST['Cijfer'];
+        $Datum = $_POST['Datum'];
+        $Toetsnaam = $_POST['Toetsnaam'];
+        $studentenID = $_POST['StudentenID'];
+        $LerarenID = $_POST['LerarenID'];
+
+        $stmt->execute();
+        $stmt->close();
 
     } catch (Exception $e) {
         echo $e->getMessage();
--- Pages/StudentGrades/Engels.php
+++ Pages/StudentGrades/Engels.php
@@ -4,11 +4,13 @@ class Engels{
     public function EngelsResultaten($mysqli)
     {
 
-        $sql = "SELECT studenten.ID,studenten.Naam, engels.Cijfer, engels.Datum FROM ( engels INNER JOIN studenten ON engels.Studenten_ID = studenten.ID);";
+        $sql = "SELECT studenten.ID,studenten.Naam, engels.Cijfer, engels.toetsNaam, engels.Datum FROM ( engels INNER JOIN studenten ON engels.Studenten_ID = studenten.ID);";
         if ($stmt = $mysqli->prepare($sql)) {
             $stmt->execute();
 
-            $stmt->bind_result($ID, $naam, $cijfer, $datum);
+            $stmt->bind_result($ID, $naam, $cijfer, $testname, $datum);
+
+
 
             echo "
             <table>
@@ -16,6 +18,7 @@ class Engels{
                 <tr>
                     <td>Naam</td>
                     <td>Cijfer</td>
+                    <td>Toets naam</td>
                     <td>Datum</td>
                 </tr>
                 </thead>
@@ -26,6 +29,7 @@ class Engels{
        <tr>
         <td>" . $naam . "</td>
         <td>" . $cijfer . "</td>
+         <td>" . $testname . "</td>
         <td>" . $datum . "</td>
         <td><a href='../Edits/Wijzigen.php?id= " . $ID . " '>Wijzigen?</a></td>
        </tr>";
